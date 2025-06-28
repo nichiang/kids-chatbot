bf9db1a8c098472e7f19aa8047b306c351878eb1
Bonnie <yuqiaoyan@gmail.com>
2025-06-27 19:48:05 -0700

    generates full prompt based on intro_prompt, vocab, and story_steps_prompt

--- backend/generate_prompt.py
+++ backend/generate_prompt.py
@@ -10,10 +10,14 @@ def load_vocabulary(csv_path):
         for row in reader: #the file holds everything in a single row
             return row
             
-
-if __name__ == "__main__":
+def generate_prompt():
     intro_system_role_text = load_file("intro_prompt.txt")
     vocab_list = load_vocabulary("vocab.csv")
-    vocab_text=",".join(vocab_list)
+    vocab_text =",".join(vocab_list)
+    vocab_section = f"Here is an example list of vocabulary words for a 2nd or 3rd grader: {vocab_text} "
     story_steps_text = load_file("story_steps_prompt.txt")
-    print(story_steps_text)
+    full_prompt = "\n".join([intro_system_role_text,vocab_section,story_steps_text])
+    return(full_prompt)
+
+if __name__ == "__main__":
+    print(generate_prompt())
